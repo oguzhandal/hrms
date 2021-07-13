e9d842a83a07d6a1e459496ec7404328b247f21a
oguzhandal <daloguz11@hotmail.com>
2021-07-13 05:23:14 +0300

    New requests repairs in the project

--- src/main/java/project/hrms/business/abstracts/JobSeekerService.java
+++ src/main/java/project/hrms/business/abstracts/JobSeekerService.java
@@ -3,11 +3,22 @@ package project.hrms.business.abstracts;
 import project.hrms.core.utilities.results.DataResult;
 import project.hrms.core.utilities.results.Result;
 import project.hrms.entities.concretes.JobSeeker;
+import project.hrms.entities.dtos.JobSeekerCvDto;
 
 import java.util.List;
 
 public interface JobSeekerService {
+    Result add(JobSeeker jobseeker);
+
+    Result update(JobSeeker jobseeker);
+
+    Result delete(int id);
+
+    DataResult<JobSeeker> getById(int id);
+
     DataResult<List<JobSeeker>> getAll();
 
-    Result add(JobSeeker jobSeeker);
+    DataResult<JobSeeker> findJobSeekerByIdentityNumber(String IdentityId);
+
+    DataResult<JobSeekerCvDto> getJobSeekerCvById(int id);
 }
--- src/main/java/project/hrms/business/concretes/JobSeekerManager.java
+++ src/main/java/project/hrms/business/concretes/JobSeekerManager.java
@@ -2,58 +2,86 @@ package project.hrms.business.concretes;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import project.hrms.business.abstracts.JobSeekerService;
-import project.hrms.core.utilities.results.*;
+import project.hrms.business.abstracts.*;
+import project.hrms.core.utilities.results.DataResult;
+import project.hrms.core.utilities.results.Result;
+import project.hrms.core.utilities.results.SuccessDataResult;
+import project.hrms.core.utilities.results.SuccessResult;
 import project.hrms.dataAccess.abstracts.JobSeekerDao;
 import project.hrms.entities.concretes.JobSeeker;
-import project.hrms.verification.abstracts.EmailCheckService;
-import project.hrms.verification.abstracts.MernisCheckService;
+import project.hrms.entities.dtos.JobSeekerCvDto;
 
 import java.util.List;
 
 @Service
 public class JobSeekerManager implements JobSeekerService {
-    private JobSeekerDao jobSeekerDao;
-    private MernisCheckService mernisCheckService;
-    private EmailCheckService emailCheckService;
 
-    @Autowired
-    public JobSeekerManager(JobSeekerDao jobSeekerDao, MernisCheckService mernisCheckService, EmailCheckService emailCheckService) {
-        this.jobSeekerDao = jobSeekerDao;
-        this.mernisCheckService = mernisCheckService;
-        this.emailCheckService = emailCheckService;
-    }
+	private JobSeekerDao jobseekerDao;
+	private ExperienceForCVService experienceForCVService;
+	private ForeignLanguageForCVService foreignLanguageForCVService;
+	private ImageForCVService imageForCVService;
+	private LinkForCVService linkForCVService;
+	private ProgrammingSkillForCVService programmingSkillForCVService;
+	private SchoolForCVService schoolForCVService;
 
+	public JobSeekerManager(JobSeekerDao jobseekerDao, ExperienceForCVService experienceForCVService,
+							ForeignLanguageForCVService foreignLanguageForCVService, ImageForCVService imageForCVService,
+							LinkForCVService linkForCVService, ProgrammingSkillForCVService programmingSkillForCVService,
+							SchoolForCVService schoolForCVService) {
+		super();
+		this.jobseekerDao = jobseekerDao;
+		this.experienceForCVService = experienceForCVService;
+		this.foreignLanguageForCVService = foreignLanguageForCVService;
+		this.imageForCVService = imageForCVService;
+		this.linkForCVService = linkForCVService;
+		this.programmingSkillForCVService = programmingSkillForCVService;
+		this.schoolForCVService = schoolForCVService;
+	}
 
-    @Override
-    public DataResult<List<JobSeeker>> getAll() {
-        return new SuccessDataResult<List<JobSeeker>>(this.jobSeekerDao.findAll(), "İş arayanlar listelendi.");
-    }
+	@Override
+	public Result add(JobSeeker jobseeker) {
+		this.jobseekerDao.save(jobseeker);
+		return new SuccessResult("JobSeeker has been added.");
+	}
 
-    @Override
-    public Result add(JobSeeker jobSeeker) {
-        if (jobSeeker.getFirstName() == null || jobSeeker.getLastName() == null ||
-                jobSeeker.getEmail() == null || jobSeeker.getPassword() == null ||
-                jobSeeker.getIdentityNumber() == null || jobSeeker.getBirthYear() == null) {
-            return new ErrorResult("Lütfen Tüm alanları eksiksiz doldurunuz");
-        } else if (!ceckcEmail(jobSeeker.getEmail())) {
-            return new ErrorResult("Email kullanılmamaktadır");
-        } else if (!checkIdentityNumber(jobSeeker.getIdentityNumber())) {
-            return new ErrorResult("Geçersiz Kimlik numarası");
-        } else if (!this.mernisCheckService.checkIfRealPerson(jobSeeker)) {
-            return new ErrorResult("Mernis doğrulaması başarısız!");
-        } else if (!this.emailCheckService.checkIfRealEmail(jobSeeker)) {
-            return new ErrorResult("Geçersiz Email!");
-        }
-        this.jobSeekerDao.save(jobSeeker);
-        return new SuccessResult("Kayıt başarılı.");
-    }
+	@Override
+	public Result update(JobSeeker jobseeker) {
+		this.jobseekerDao.save(jobseeker);
+		return new SuccessResult("JobSeeker has been updated.");
+	}
 
-    private boolean ceckcEmail(String email) {
-        return this.jobSeekerDao.getByEmail(email) == null;
-    }
+	@Override
+	public Result delete(int id) {
+		this.jobseekerDao.deleteById(id);
+		return new SuccessResult("JobSeeker has been deleted.");
+	}
 
-    private boolean checkIdentityNumber(String identityNumber) {
-        return this.jobSeekerDao.getByIdentityNumber(identityNumber) == null;
-    }
+	@Override
+	public DataResult<JobSeeker> getById(int id) {
+		return new SuccessDataResult<JobSeeker>(this.jobseekerDao.getById(id));
+	}
+
+	@Override
+	public DataResult<List<JobSeeker>> getAll() {
+		return new SuccessDataResult<List<JobSeeker>>(this.jobseekerDao.findAll());
+	}
+
+	@Override
+	public DataResult<JobSeeker> findJobSeekerByIdentityNumber(String identityNumber) {
+		return new SuccessDataResult<JobSeeker>(this.jobseekerDao.findJobSeekerByIdentityNumber(identityNumber));
+	}
+
+	@Override
+	public DataResult<JobSeekerCvDto> getJobSeekerCvById(int id) {
+		JobSeeker jobseeker = this.jobseekerDao.getById(id);
+		JobSeekerCvDto cv = new JobSeekerCvDto();
+		cv.experiences = jobseeker.getExperiences();
+		cv.languages = jobseeker.getLanguages();
+		cv.image = jobseeker.getImage();
+		cv.links = jobseeker.getLinks();
+		cv.programingSkills = jobseeker.getProgramingSkills();
+		cv.schools = jobseeker.getSchools();
+		return new SuccessDataResult<JobSeekerCvDto>(cv);
+	}
+
 }
